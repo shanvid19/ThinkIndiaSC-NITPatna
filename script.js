1faef8a30bc099f083c135872417cd8bac257b4a
shanvid19 <143689484+shanvid19@users.noreply.github.com>
2024-07-04 15:44:36 +0530

    Enhancements in Burger Menu

--- script.js
+++ script.js
@@ -3,9 +3,12 @@ navbar= document.querySelector(".nav-bar");
 
 burger.addEventListener('click',()=>{
     if (navbar.style.display === "none") { 
-        navbar.style.display = "flex"; 
+        navbar.style.display = "flex";
+        burger.style.position = "fixed"; 
+        navbar.style.position = "fixed";
     } 
     else { 
         navbar.style.display = "none"; 
     }
 })
+
